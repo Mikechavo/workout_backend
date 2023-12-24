157c9e91d3c3172f96eae8c51aa8e668ab550b9d
Mikechavo <mchavez76@yahoo.com>
2023-12-24 01:47:25 -0600

    routine spec edit

--- spec/models/routine_spec.rb
+++ spec/models/routine_spec.rb
@@ -8,17 +8,19 @@ RSpec.describe Routine, type: :model do
     )
   }
 
-    it 'should validate muscle group' do
-      routine = user.routines.create(
-        muscle_group: 'Tricep',
-        sets: 4,
-        reps: 15,
-        weight: 180,
-        image: "https://cdn5.vectorstock.com/i/1000x1000/49/69/man-doing-cable-tricep-pull-down-exercise-vector-35544969.jpg",
-        notes: 'Fully extend'
-      )
+  it 'should validate muscle group' do
+    routine = user.routines.build(
+      sets: 4,
+      reps: 15,
+      weight: 180,
+      image: "https://cdn5.vectorstock.com/i/1000x1000/49/69/man-doing-cable-tricep-pull-down-exercise-vector-35544969.jpg",
+      notes: 'Fully extend'
+    )
 
-      expect(routine.errors[:muscle_group]).to include("can't be blank")
-    end
+    # Note: Omitting muscle_group from the routine creation
+
+    expect(routine.valid?).to be false
+    expect(routine.errors[:muscle_group]).to include("can't be blank")
   end
+end
 
